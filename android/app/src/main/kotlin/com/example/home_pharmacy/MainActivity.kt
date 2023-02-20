8052eef0e244c83501e8944d3a4f5c5952d29404
Angelo Guzzo <angelo.guzzo83@gmail.com>
2023-02-20 10:19:15 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/home_pharmacy/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.home_pharmacy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/model/drug.dart
@@ -0,0 +1,6 @@
+class Drug {
+  final String id;
+  final String name;
+
+  Drug(this.id, this.name);
+}
